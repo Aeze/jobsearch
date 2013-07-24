32f92959d91150654f5f661963c063f3c2a6a068
Marcus Orochena <marcus.orochena@gmail.com>
2013-07-24 15:54:49 -0400

    auth

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,8 @@
 var app = angular.module('cosmop', ['ui.bootstrap','ngResource']);
 
 app.config(["$httpProvider", function(provider) {
-    provider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-  }]);
+  provider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+}]);
 
 app.factory('Jobs', function($resource) {
   return $resource("/jobs/:id", {id: "@id"}, {update: {method: "PUT"}});
@@ -11,36 +11,34 @@ app.factory('Jobs', function($resource) {
 app.filter('startFrom', function() {
     return function(input, start) {
         if(input) {
-            start = +start; //parse to int
-            return input.slice(start);
-        }
-        return [];
-    }
+start = +start; //parse to int
+return input.slice(start);
+}
+return [];
+}
 });
 
 
 var JobsCtrl = function($scope, $timeout, Jobs) {
-	 $scope.list = Jobs.query(function() {
-	 	$scope.currentPage = 1; //current page
-    $scope.maxSize = 5; //pagination max size
-    $scope.entryLimit = 5; //max rows for data table
-    var initializing = true;
+    $scope.list = Jobs.query(function() {
+$scope.currentPage = 1; //current page
+$scope.maxSize = 5; //pagination max size
+$scope.entryLimit = 5; //max rows for data table
+var initializing = true;
 
-    /* init pagination with $scope.list */
-    $scope.noOfPages = Math.ceil($scope.list.length/$scope.entryLimit);
-    $scope.setPage = function(pageNo) {
-        $scope.currentPage = pageNo;
-    };
+/* init pagination with $scope.list */
+$scope.noOfPages = Math.ceil($scope.list.length/$scope.entryLimit);
+$scope.setPage = function(pageNo) {
+    $scope.currentPage = pageNo;
+};
 
-    $scope.$watch('search', function() {
-    	if(initializing) {
-    		 $timeout(function() { initializing = false; });
-    	} else {
-    		$scope.noOfPages = Math.ceil($scope.filteredList.length/$scope.entryLimit);
-    	}
-    });
+$scope.$watch('search', function() {
+    if(initializing) {
+        $timeout(function() { initializing = false; });
+    } else {
+        $scope.noOfPages = Math.ceil($scope.filteredList.length/$scope.entryLimit);
+    }
+});
 
-	 });
-
-
+});
 }
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   def home
     @jobs = Job.all
   end
+
+  def admin
+    @jobs = Job.all
+    @users = User.all
+  end
 end
