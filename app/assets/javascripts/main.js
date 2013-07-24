489e50fc5427592726bec384c6f2b58aa18e09d2
Omar <omar@ubuntu.(none)>
2013-07-24 18:37:50 -0400

    Markers Songit add .

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -20,7 +20,9 @@ app.filter('startFrom', function() {
 
 
 var JobsCtrl = function($scope, $timeout, Jobs) {
-	 $scope.list = Jobs.query(function() {
+
+	 var list = Jobs.query(function() {
+	 	$scope.list = list;
 	 	$scope.currentPage = 1; //current page
     $scope.maxSize = 5; //pagination max size
     $scope.entryLimit = 5; //max rows for data table
@@ -44,3 +46,54 @@ var JobsCtrl = function($scope, $timeout, Jobs) {
 
 
 }
+
+app.directive('map', function($timeout){
+
+	return {
+		restrict: 'E',
+		template: "<div id='map'></div>",
+		link: function(scope, element, attrs) {
+			var map = L.mapbox.map('map', 'examples.map-uci7ul8p').setView([40, -74.50], 9);
+
+	    document.getElementById('map-navigation').onclick = function(e) {
+		    var pos = e.target.getAttribute('data-position');
+		    if (pos) {
+		        var loc = pos.split(',');
+		        map.setView(loc, 11);
+		    }
+			};
+			var loading = true;
+			scope.$watch(attrs.ngModel, function(data) {
+				console.log(data);
+				if(loading) {
+    		 $timeout(function() { loading = false; });
+    		} else {
+					for(var i=0;i<data.length;i++){
+						console.log(data[i].latitude);
+						var lat = data[i].latitude;
+						var lon = data[i].longitude;
+						L.mapbox.markerLayer({
+					    // this feature is in the GeoJSON format: see geojson.org
+					    // for the full specification
+					    type: 'Feature',
+					    geometry: {
+					        type: 'Point',
+					        // coordinates here are in longitude, latitude order because
+					        // x, y is the standard for GeoJSON and many formats
+					        coordinates: [lon, lat]
+					    },
+					    properties: {
+					        title: 'A Single Marker',
+					        description: 'Just one of me',
+					        // one can customize markers by adding simplestyle properties
+					        // http://mapbox.com/developers/simplestyle/
+					        'marker-size': 'large',
+					        'marker-color': '#f0a'
+					    }
+						}).addTo(map);
+					}
+    		}
+			});
+		}
+	};
+});
